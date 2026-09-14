474a0eecc50107850e0970ac6930dbe48ba87751
maleekanas <33572878+maleekanas@users.noreply.github.com>
2026-09-14 14:22:46 +0200

    Update base URL format in sitemap function
    Change base URL to omit 'https://' in sitemap.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 import { locales } from "@/lib/localization";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.arabickidsacademy.com";
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "www.arabickidsacademy.com";
 
   const publicRoutes = ["", "/login", "/privacy", "/terms"];
 
